9f78e2e125d656d07ee3089295212c1aea703ef8
Augusto359 <augustocealmeida2010@gmail.com>
2026-08-16 14:42:54 -0300

    feat

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/analisar-planta': 'http://localhost:5000', // Encaminha requisições do front para o Node
-    },
-  },
-});
+    host: true,          // Permite que o FrontEnd escute conexões além do localhost
+    allowedHosts: true   // Cria a liberação de segurança para o ngrok parar de dar o erro 403
+  }
+})
